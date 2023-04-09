daa30b686e2da5a1f5f66dd18e713dd3aff16f92
anupsharma <anupnewsmail@gmail.com>
2023-04-10 02:17:00 +0530

    Added few macros and structure related to row, tables to handle single table database
    Signed-off-by: anupsharma <anupnewsmail@gmail.com>

--- main.c
+++ main.c
@@ -2,7 +2,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define COLUMN_USERNAME_SIZE 32
+#define COLUMN_EMAIL_SIZE 255
+#define size_of_attribute(Struct, Attribute) sizeof(((struct*)0)->Attribute)
 
+#define TABLE_MAX_PAGES 100
+const uint32_t PAGE_SIZE = 4096;
+const uint32_t ROWS_PER_PAGE = PAGE_SIZE / ROW_SIZE;
+const uint32_t TABLE_MAX_ROWS = ROWS_PER_PAGE * TABLE_MAX_PAGES;
+
+const uint32_t ID_SIZE = size_of_attribute(Row, id);
+const uint32_t USERNAME_SIZE = size_of_attribute(Row, username);
+const uint32_t EMAIL_SIZE = size_of_attribute(Row, email);
+const uint32_t OFFSET = 0; 
+const uint32_t USERNAME_OFFSET = ID_OFFSET + ID_SIZE;
+const uint32_t EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE;
+const uint32_t ROW_SIZE = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE;
+
+void serialize_row(Row* source, void* destination) {
+	memcpy(destination + ID_OFFSET, &(source->id), ID_SIZE);
+	memcpy(destination + USERNAME_OFFSET, &(source->username), USERNAME_SIZE);
+	memcpy(destination + EMAIL_OFFSET, &(source->email), EMAIL_SIZE);
+}
+void deserialize_row(void* source, Row* destination) {
+	memcpy(&(destination->id), source + ID_OFFSET, ID_SIZE);
+	memcpy(&(destination->username), source + USERNAME_OFFSET, USERNAME_SIZE);
+	memcpy(&(destination->email), source + EMAIL_OFFSET, EMAIL_SIZE);
+}
+typedef struct {
+	uint32_t id;
+	char username[COLUMN_USERNAME_SIZE];
+	char email[COLUMN_EMAIL_SIZE];
+} Row;
+
+typedef struct {
+	uint32_t num_rows;
+	void* page[TABLE_MAX_PAGES];
+} Table;
 typedef struct {
   char* buffer;
   size_t buffer_length;
@@ -18,7 +54,64 @@ InputBuffer* new_input_buffer() {
   return input_buffer;
 }
 
-void print_prompt() { printf("db > "); }
+typedef enum {
+	META_COMMAND_SUCCESS,
+	META_COMMAND_UNRECOGNIZED
+} MetaCommandResult;
+
+typedef enum {
+	PREPARE_SUCCESS,
+	PREPARE_UNRECOGNIZED_STATEMENT
+}PrepareResult;
+
+MetaCommandResult do_meta_command(InputBuffer* input_buffer) {
+	if (strcmp(input_buffer->buffer, ".exit") == 0 ) {
+		exit(EXIT_SUCCESS);
+	}
+	else {
+		return META_COMMAND_UNRECOGNIZED;
+	}
+}
+typedef enum {
+	STATEMENT_INSERT, 
+	STATEMENT_SELECT
+} StatementType;
+
+typedef struct {
+	StatementType type;
+	Row row_to_insert; //only used by insert statement
+} Statement;
+
+PrepareResult prepare_statment(InputBuffer* input_buffer, Statement* statement) {
+	if (strncmp(input_buffer->buffer, "insert", 6) == 0) {
+	statement->type = STATEMENT_INSERT;
+	int args_assigned = sscanf(
+		input_buffer->buffer, "Insert %d %s %s", &(statement->row_to_insert.id),
+		statement->row_to_insert.username, statement->row_to_insert.email);
+	if (args_assigned < 3) {
+		return PREPARE_SYNTAX_ERROR;
+	}
+	return PREPARE_SUCCESS;
+	}
+	if (strcmp(input_buffer->buffer, "select") == 0) {
+	statement->type = STATEMENT_SELECT;
+	return PREPARE_SUCCESS;
+	}
+	return PREPARE_UNRECOGNIZED_STATEMENT;
+}
+void execute_statement(Statement* statement) {
+	switch(statement->type) {
+		case(STATEMENT_INSERT):
+			printf("This is where we would do an insert\n");
+			break;
+		case(STATEMENT_SELECT):
+			printf("This is where we would do select\n");
+			break;
+		}
+}
+void print_prompt() { 
+	printf("db > "); 
+}
 
 void read_input(InputBuffer* input_buffer) {
   ssize_t bytes_read =
@@ -45,11 +138,26 @@ int main(int argc, char* argv[]) {
     print_prompt();
     read_input(input_buffer);
 
-    if (strcmp(input_buffer->buffer, ".exit") == 0) {
-      close_input_buffer(input_buffer);
-      exit(EXIT_SUCCESS);
-    } else {
-      printf("Unrecognized command '%s'.\n", input_buffer->buffer);
+printf("The input_buffer is %s\n", input_buffer->buffer);
+if (strcmp(input_buffer->buffer, ".exit") == 0) {
+	if (input_buffer->buffer[0] == ".") {
+		switch(do_meta_command(input_buffer)) {
+			case (META_COMMAND_SUCCESS):
+				continue;
+			case (META_COMMAND_UNRECOGNIZED):
+				printf("Unrecognized command '%s'.\n", input_buffer->buffer);
+		}
+	}
+	Statement statement;
+	switch (prepare_statment(input_buffer, &statement)) {
+		case (PREPARE_SUCCESS):
+			break;
+		case (PREPARE_UNRECOGNIZED_STATEMENT):
+			printf("Unrecognized keyword at start of '%s'. \n", input_buffer->buffer);
+			continue;
+	}
+	execute_statement(&statement);
+	printf("Executed\n");
     }
   }
 }
